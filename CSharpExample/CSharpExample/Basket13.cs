dd336cf418fc35c18ae5478d87c85f9497bfba1f
WeraP <36221806+WeraP@users.noreply.github.com>
2018-03-06 06:51:12 +0100

    Lesson 7 exec 13

---
+++ CSharpExample/CSharpExample/Basket13.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using OpenQA.Selenium.Firefox;
+
+namespace CSharpExample
+{
+	[TestFixture]
+	public class Basket13 : General
+	{
+		[Test]
+		public void Basket()
+		{
+			int itemsQty = 0;
+			int qtyFirst = 0;
+
+			while (itemsQty < 3)
+			{
+				driver.Url = "http://localhost:8080/litecart/en/";
+				wait.Until(ExpectedConditions.TitleIs("Online Store | My Store"));
+
+
+				driver.FindElement(By.CssSelector(".image-wrapper")).Click();
+				wait.Until(ExpectedConditions.ElementExists(By.CssSelector("#box-product")));
+
+				IWebElement qtyElement = driver.FindElement(By.CssSelector("span.quantity"));
+				qtyFirst = Convert.ToInt32(qtyElement.GetAttribute("textContent"));
+
+
+				//Add To Cart
+
+				if (driver.FindElements(By.CssSelector("select[name*=options]")).Count > 0)
+				{
+					var size = new SelectElement(driver.FindElement(By.CssSelector("select[name*=options]")));
+					size.SelectByIndex(1);
+				}
+				driver.FindElement(By.CssSelector("button[name=add_cart_product]")).Click();
+				wait.Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(By.CssSelector("span.quantity[style]")));
+
+				//wait.Until(ExpectedConditions.ElementExists(By.CssSelector("span.quantity")));
+				int qty = Convert.ToInt32(driver.FindElement(By.CssSelector("span.quantity")).GetAttribute("textContent"));
+				Assert.AreEqual(1, qty - qtyFirst);
+				itemsQty++;
+			}
+
+				driver.FindElement(By.CssSelector("a.link[href*=checkout]")).Click();
+			wait.Until(ExpectedConditions.ElementExists(By.CssSelector("td.item")));
+
+			int ItemsInCart = driver.FindElements(By.CssSelector("td.item")).Count;
+			Debug.WriteLine(ItemsInCart);
+
+			while (ItemsInCart > 0)
+			{
+				driver.FindElement(By.CssSelector("button[name = remove_cart_item]")).Click();
+				wait.Until(ExpectedConditions.StalenessOf(driver.FindElements(By.CssSelector("td.item"))[0]));
+				ItemsInCart--;
+			}
+
+
+		}
+
+
+
+	}
+}
---
+++ CSharpExample/CSharpExample/General.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using System.Diagnostics;
+
+namespace CSharpExample
+{
+	[TestFixture]
+	public class General
+	{
+		protected IWebDriver driver;
+		protected WebDriverWait wait;
+
+		[SetUp]
+		public void Start()
+		{
+			driver = new ChromeDriver();
+			wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+		}
+
+				private void LoginAdmin()
+		{
+			driver.Url = "http://localhost:8080/litecart/admin/login.php";
+			driver.FindElement(By.Name("username")).SendKeys("admin");
+			driver.FindElement(By.Name("password")).SendKeys("admin");
+			driver.FindElement(By.Name("login")).Click();
+			wait.Until(ExpectedConditions.TitleIs("My Store"));
+		}
+
+		[TearDown]
+		public void Stop()
+		{
+			driver.Quit();
+			driver = null;
+		}
+	}
+}
