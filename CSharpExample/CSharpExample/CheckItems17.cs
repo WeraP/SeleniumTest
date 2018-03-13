65b188f20432f4421ad84547eca870b0053f3d15
WeraP <36221806+WeraP@users.noreply.github.com>
2018-03-13 11:44:47 +0100

    Lesson 10 ex 17

---
+++ CSharpExample/CSharpExample/CheckItems17.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.Events;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using OpenQA.Selenium.Firefox;
+
+namespace CSharpExample
+{
+	[TestFixture]
+	public class CheckItems17 : GeneralFiring
+	{
+		[Test]
+		public void CheckItems()
+		{
+			LoginAdmin();
+
+			var logTypes = driver.Manage().Logs.AvailableLogTypes;
+
+			driver.Url = "http://localhost:8080/litecart/admin/?app=catalog&doc=catalog&category_id=1";
+			wait.Until(ExpectedConditions.TitleIs("Catalog | My Store"));
+			
+			int itemsQty = driver.FindElements(By.CssSelector("td:nth-child(3) > a[href*=product_id]")).Count;
+
+			for (var i = 0; i < itemsQty; i++)
+			{
+				driver.FindElements(By.CssSelector("td:nth-child(3) > a[href*=product_id]"))[i].Click();
+
+				foreach (var logType in logTypes)
+				{
+					var qtyLog = driver.Manage().Logs.GetLog(logType).Count;
+					Console.WriteLine(qtyLog + " Qty;" + logType + " Type");
+
+					foreach (LogEntry l in driver.Manage().Logs.GetLog(logType))
+					{
+						Console.WriteLine(l);
+					}
+				}
+				driver.Url = "http://localhost:8080/litecart/admin/?app=catalog&doc=catalog&category_id=1";
+			}
+			
+		}
+	}
+}
+
---
+++ CSharpExample/CSharpExample/GeneraFiring.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.Events;
+
+namespace CSharpExample
+{
+	[TestFixture]
+	public class GeneralFiring
+	{
+		protected EventFiringWebDriver driver;
+		protected WebDriverWait wait;
+
+		[SetUp]
+		public void Start()
+		{
+			ChromeOptions options = new ChromeOptions();
+			options.SetLoggingPreference(LogType.Browser, LogLevel.All);
+			
+			driver = new EventFiringWebDriver(new ChromeDriver());
+			//driver.FindingElement += driver_FindingElement;
+			//driver.FindElementCompleted += (sender,e) => Console.WriteLine(e.FindMethod + " found");
+			//driver.ExceptionThrown += (sender, e) => Console.WriteLine(e.ThrownException);
+			wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+		}
+
+		void driver_FindingElement(object sender, FindElementEventArgs e)
+		{
+			Console.WriteLine(e.FindMethod);
+		}
+
+		public void LoginAdmin()
+		{
+			driver.Url = "http://localhost:8080/litecart/admin/login.php";
+			driver.FindElement(By.Name("username")).SendKeys("admin");
+			driver.FindElement(By.Name("password")).SendKeys("admin");
+			driver.FindElement(By.Name("login")).Click();
+			wait.Until(ExpectedConditions.TitleIs("My Store"));
+		}
+
+		public static Func<IWebDriver, string> ThereIsWindowOtherThan(IEnumerable<string> oldWindows)
+		{
+			return driver => driver.WindowHandles.Except(oldWindows).ToList().Single();
+		}
+
+		[TearDown]
+		public void Stop()
+		{
+			driver.Quit();
+			driver = null;
+		}
+	}
+}
